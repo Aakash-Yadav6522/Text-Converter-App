690a80ac587b2d07f946061728cfebb3c679919b
Aakash Yadav <137447879+Aakash-Yadav6522@users.noreply.github.com>
2024-03-01 02:29:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import React,{useState} from 'react'
+import Alert from './Components/Alert';
+
+function App() {
+  const [mode, setMode] = useState('light');
+  const [attension, setAttension] = useState(null)
+  const showAlert=(message,type)=>{
+    setAttension({
+      msg:message,
+      type:type
+    });
+    setTimeout(() => {
+      setAttension(null);
+    }, 1000);
+  }
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='rgb(110 118 131)';
+      showAlert("Dark Mode","success");
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor='white';
+      showAlert("Light Mode","success");
+    }
+  }
+  return (
+    <>
+     <Navbar title="TextConverter" mode={mode} toggleMode={toggleMode}/>
+     <Alert attension={attension}/>
+     <div className="container my-3">
+     {<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}
+     </div>
+    </>
+  );
+}
+export default App;
